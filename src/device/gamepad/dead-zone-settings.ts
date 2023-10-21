4b2017379b07a6514ad24347faafe31dd5129395
electrovir <electrovir@users.noreply.github.com>
2023-10-21 17:35:18 +0000

    simplify GamepadDeadZoneSettings to just a map from device names to input names to values

--- src/device/gamepad/dead-zone-settings.ts
+++ src/device/gamepad/dead-zone-settings.ts
@@ -1,13 +1,8 @@
-export type GamepadDeadZoneSettings = Readonly<
-    Record<
-        string,
-        {
-            axesDeadZones: Readonly<Record<number, number>>;
-            /**
-             * Button dead zones are applicable to pressure sensitive buttons, like triggers. Note
-             * that sometimes pressure sensitive buttons map to axes instead of buttons.
-             */
-            buttonDeadZones: Readonly<Record<number, number>>;
-        }
-    >
->;
+export type GamepadDeadZoneSettings = Readonly<{
+    [deviceName: string]: {
+        [
+            inputName: string
+        ]: /** The minimum value, in either direction for an input to be considered as triggering an action. */
+        number;
+    };
+}>;
